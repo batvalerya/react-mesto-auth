de7be0aeb337e79a8b1f0be42afd0c80e38d9614
Baturina Valeriya <bat.valerya@yandex.ru>
2022-08-12 18:20:19 +0700

    изменить вид шапки для авторизованного и неавторизованного пользователя

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
 import logo from '../images/logo.svg';
+import { Link, Route } from 'react-router-dom';
 
 function Header() {
     return(
         <header className="header">
             <img src={logo} alt="Логотип" className="header__logo" />
+            <Route path="/sign-up">
+                <Link to="/sign-in" className='header__link'>Войти</Link>
+            </Route>
+
+            <Route path="/sign-in">
+                <Link to="/sign-up" className='header__link'>Регистрация</Link>
+            </Route>
+
+            <Route path="/main">
+                <div className='header__info'>
+                    <a className='header__email' href="#">bat.valerya@yandex.ru</a>
+                    <Link to="/" className='header__link-logout'>Выйти</Link>
+                </div>
+            </Route>
+            
         </header>
     )
 };
